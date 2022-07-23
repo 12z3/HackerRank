92bc95b320cb46736a2f231bc94123ff02638aab
12z3 <77529497+12z3@users.noreply.github.com>
2022-07-23 07:03:51 +0300

    Add files via upload

--- RotatedArray.java
+++ RotatedArray.java
@@ -2,23 +2,14 @@ package hackerRank;
 
 import training.Methods;
 
+import java.util.Arrays;
+
 public class RotatedArray extends Methods {
-    public static void main(String[] args) {
-
-        /**
-         * https://javarevisited.blogspot.com/2015/07/
-         how-to-rotate-array-in-java-leetcode-solution.html?utm_source=dlvr.it&utm_medium=facebook
-         * Java Program to rotate array in Java
-         * Problem : Rotate an integer array of n elements after kth index.
-         * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7]
-         * is rotated to [5,6,7,1,2,3,4]
-         *
-         * @author Javin Paul
-         */
-
-        int k = 2;
-        int[] a = {1, 2, 3, 4, 5, 6, 7};
+    private static int[] rotatedArray(int[] a, int k) {
         int[] tmp = new int[a.length];
+        boolean isBreak = false;
+
+        if (k >= a.length) return new int[a.length];
 
         for (int i = k + 1, j = 0; i < a.length; i++, j++) {
             tmp[j] = a[i];
@@ -26,6 +17,27 @@ public class RotatedArray extends Methods {
         for (int i = a.length - 1 - k, j = 0; i < a.length; i++, j++) {
             tmp[i] = a[j];
         }
+        return tmp;
+    }
+
+    public static void main(String[] args) {
+
+        /**
+         * https://javarevisited.blogspot.com/2015/07/
+         how-to-rotate-array-in-java-leetcode-solution.html?utm_source=dlvr.it&utm_medium=facebook
+
+         * Problem : Rotate an integer array of n elements after kth index.
+         * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7]
+         * is rotated to [5,6,7,1,2,3,4]
+         *
+         * @author Javin Paul
+         */
+
+        int k = 3;
+        int[] a = {1, 2, 3, 4, 5, 6, 7};
+        int[] tmp = new int[a.length];
+
+        System.out.println(Arrays.toString(rotatedArray(a, k)));
 
 //        for (int i = k + 1; i < a.length; i++) {
 //            if (j <= k + 1) {
@@ -40,9 +52,5 @@ public class RotatedArray extends Methods {
 //                j++;
 //            }
 //        }
-
-        printIntArray(tmp);
-
-
     }
 }
