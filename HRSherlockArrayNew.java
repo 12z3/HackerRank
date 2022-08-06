ff76f6a53f713046fd3d34a5affe7728c601a435
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-06 20:12:07 +0300

    Add files via upload

--- HRSherlockArrayNew.java
+++ HRSherlockArrayNew.java
@@ -28,13 +28,14 @@ public class HRSherlockArrayNew extends Methods {
         int[] left = new int[input.length], right = new int[input.length];
         int sumL = 0, sumR = 0, sum = 0;
 
-        System.out.println(  sherlock(input));
+        System.out.println(sherlock(input));
     }
 
     /**
-       @Idea: Динамично разделя входният масив на две части и търси сумата от елементите им.
-     * @param input
-     * @return
+     * @param int[] input
+     * @return String: "YES" or "NO".
+     * @Idea: Динамично разделя входният масив на две части и търси сумата от елементите им.
+     * @Issue: На всяка итерация се съдават по два Нови масива.
      */
 
     private static String sherlock(int[] input) {
@@ -44,7 +45,7 @@ public class HRSherlockArrayNew extends Methods {
 
         for (int i = 0; i < input.length; i++) {
             sumL = sumR = 0;
-            if (i == 0){
+            if (i == 0) {                                       // Ако "i" = 0 -> Първи граничен случай
                 sumL = input[i];
                 for (int j = 1; j < input.length; j++) {
                     sumR += input[j];
@@ -52,29 +53,29 @@ public class HRSherlockArrayNew extends Methods {
                 if (sumL == sumR) {
                     return "Yes";
                 } else continue;
-            } else if (i == input.length - 1){
+            } else if (i == input.length - 1) {                  // Ако "i" = последното число -> Втори граничен случай
                 sumR = input[input.length - 1];
-                for (int j = input.length - 2; j >= 0 ; j--) {
+                for (int j = input.length - 2; j >= 0; j--) {
                     sumL += input[j];
                 }
                 if (sumL == sumR) {
                     return "Yes";
                 } else return "No";
-            }else {
-            sumL = sumR = 0;
-            left = new int[i];
-            for (int j = 0; j < i; j++) {
-                left[j] = input[j];
-                sumL += left[j];
-            }
-            right = new int[input.length - i - 1];
-            for (int j = 0; j < input.length - i - 1; j++) {
-                right[j] = input[j + i + 1];
-                sumR += right[j];
-            }
-            if (sumL == sumR) {
-                isMatch = true;
-                break;
+            } else {                                             // .. за всички останали "вътрешни" числа
+                sumL = sumR = 0;                                 //  int[] input = {5, 6, 8, 11};
+                left = new int[i];                 // Размера на лявата и дясната част се определя от стойността на "i".
+                for (int j = 0; j < i; j++) {                    // В случая на i = 2 -> Лява част: -> [1,3]
+                    left[j] = input[j];
+                    sumL += left[j];
+                }
+                right = new int[input.length - i - 1];
+                for (int j = 0; j < input.length - i - 1; j++) {
+                    right[j] = input[j + i + 1];                 // В случая на i = 2 -> Дясна част: -> [5,6,7]
+                    sumR += right[j];
+                }
+                if (sumL == sumR) {
+                    isMatch = true;
+                    break;
                 }
             }
         }
@@ -90,12 +91,14 @@ public class HRSherlockArrayNew extends Methods {
 //
 //        for (int i = 0; i < arr.size(); i++) {
 //            sumL = sumR = 0;
-//            left = new int[i];
+//            //left = new int[i];
+//            left = new int[0];                                     // За да излязат тестовете.
 //            for (int j = 0; j < i; j++) {
 //                left[j] = arr.get(j);
 //                sumL += left[j];
 //            }
-//            right = new int[arr.size() - i - 1];
+//            //right = new int[arr.size() - i - 1];
+//            right = new int[0];                                     // За да излязат тестовете.
 //            for (int j = 0; j < arr.size() - i - 1; j++) {
 //                right[j] = arr.get(j + i + 1);
 //                sumR += right[j];
@@ -114,22 +117,21 @@ public class HRSherlockArrayNew extends Methods {
 //    }
 
 
-
 //    private static String sherlock(int[] input) {
 //        int sumL =0, sumR = 0;
 //        int[] right, left;
 //        boolean isMatch = false;
 //
 //        for (int i = 0; i < input.length; i++) {
-//            sumL = sumR = 0;                                               //  int[] input = {5, 6, 8, 11};
-//            left = new int[i];                     // Размера на лявата и дясната част се определя от стойността на "i".
-//            for (int j = 0; j < i; j++) {                                  // В случая на i = 2 -> [1,3]
+//            sumL = sumR = 0;
+//            left = new int[i];
+//            for (int j = 0; j < i; j++) {
 //                left[j] = input[j];
 //                sumL += left[j];
 //            }
 //            right = new int[input.length - i - 1];
 //            for (int j = 0; j < input.length - i - 1; j++) {
-//                right[j] = input[j + i + 1];                               // В случая на i = 2 -> [5,6,7]
+//                right[j] = input[j + i + 1];
 //                sumR += right[j];
 //            }
 //            if (sumL == sumR) {
