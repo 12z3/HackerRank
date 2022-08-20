a21dd71131c116c4d008cb74329686f65d446583
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-20 10:39:32 +0300

    Add files via upload

--- HRGridChallenge1.java
+++ HRGridChallenge1.java
@@ -3,16 +3,33 @@ package hackerRank;
 import java.util.*;
 
 public class HRGridChallenge1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        String[] input1 = {"mpxz", "abcd", "wlmf"};            // mpxz abcd flmw;
-        String[] input = {"ebacd", "fghij", "olmkn", "trpqs", "xywuv"};
+    /**
+     * @Problem: Given a square grid of characters in the range ascii[a-z],
+     * rearrange elements of each row alphabetically, ascending.
+     * Determine if the columns are also in ascending alphabetical order,
+     * top to bottom. Return YES if they are or NO if they are not.
+     *
+     * @Source: https://www.hackerrank.com/challenges/grid-challenge/
+     * problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+     * @Discussins: https://www.hackerrank.com/challenges/grid-challenge/forum
+     *
+     * @Input: ebacd   grid = {"ebacd", "fghij", "olmkn", "trpqs", "xywuv"}
+     * fghij
+     * olmkn
+     * trpqs
+     * xywuv
+     *
+     * @Output:
+     * YES
+     * abcde
+     * fghij
+     * klmno
+     * pqrst
+     * uvwxy
+     */
 
-
-        List<String> lists = Arrays.asList("mpxz", "abcd", "wlmf");
-        List<String> lists1 = Arrays.asList("ebacd", "fghij", "olmkn", "trpqs", "xywuv");
-
+    private static String gridChallenge1(List<String> lists) {
         StringBuilder stb = new StringBuilder();
         int countLength = 0, countNumbersOfCharInThatWord = 0;
         int countNo = 0;
@@ -54,10 +71,6 @@ public class HRGridChallenge1 {
             }
         }
 
-        if (countNo != 0) {
-            System.out.print("NO");
-        } else System.out.print("YES");
-
         System.out.println();
         for (int row = 0; row < lists.size(); row++) {
             for (int col = 0; col < lists.get(row).length(); col++) {
@@ -66,7 +79,23 @@ public class HRGridChallenge1 {
             System.out.println();
         }
 
+        if (countNo != 0) {
+            return "NO";
+        } else return "YES";
 
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input1 = {"mpxz", "abcd", "wlmf"};                                
+        String[] input = {"ebacd", "fghij", "olmkn", "trpqs", "xywuv"};
+
+
+        List<String> lists = Arrays.asList("mpxz", "abcd", "wlmf");
+        List<String> lists1 = Arrays.asList("ebacd", "fghij", "olmkn", "trpqs", "xywuv");
+
+        gridChallenge1(lists);
     }
 }
 
