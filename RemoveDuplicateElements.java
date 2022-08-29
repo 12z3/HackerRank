e41e37182a171420beab0990d82ec0cd583a40ff
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-29 10:30:56 +0300

    Add files via upload

--- RemoveDuplicateElements.java
+++ RemoveDuplicateElements.java
@@ -17,7 +17,7 @@ public class RemoveDuplicateElements extends Methods {
 
             if (i != 0) {
                 for (int j = 0; j < checkedElements.length; j++) {
-                    if (arr[i] == checkedElements[j]) {                            // Търси съвпадения
+                    if (arr[i] == checkedElements[j]) {                       // Търси съвпадения
                         isChecked = true;
                         break;
                     }
@@ -26,7 +26,7 @@ public class RemoveDuplicateElements extends Methods {
             if (isChecked) continue;                    // arr = {1, 2, 1, 1, 2, 2, 3, 4}
             // checkedElements[i] = arr[i];             // checkedElements = {1, 2, 0, 0, 0, 0, 3, 4} = non-duplicated
 
-            for (int j = i + 1; j < arr.length; j++) {             // Не стига до тук ако е проверяван елемента -
+            for (int j = i + 1; j < arr.length; j++) {     // Не стига до тук ако е проверяван елемента -
                 if (arr[i] == arr[j]) {                    // и има съвпадение -> if (arr[i] == checkedElements[j])
                     countDuplicate++;
                 }
@@ -41,16 +41,6 @@ public class RemoveDuplicateElements extends Methods {
                 tmp[index++] = checkedElements[i];
 
             }
-//        for (int i = 0; i < arr.length; i++) {
-//            mach = 1;
-//            for (int j = i + 1; j < arr.length; j++) {
-//                if (arr[i] == arr[j]) {
-//                    mach++;
-//                }
-//            }
-//            if (mach == 1) {
-//                tmp[index++] = arr[i];                 // Записва елементи с Индекси -> 1, 4, 5
-//            }                                          // Първо добавя елемента в tmp[l], след това увеличава л с 1.
         }
         arr = tmp;
         return arr;
