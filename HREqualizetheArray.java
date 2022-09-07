26c24d10a5e0b168b9fae087f6e6e960f5832e01
12z3 <77529497+12z3@users.noreply.github.com>
2022-09-07 18:17:12 +0300

    Add files via upload

--- HREqualizetheArray.java
+++ HREqualizetheArray.java
@@ -14,17 +14,17 @@ public class HREqualizetheArray {
 
     public static void main(String[] args) {
         List<Integer> input2 = new ArrayList<>(List.of(1, 2, 2, 3));
-        List<Integer> input1 = new ArrayList<>(List.of(3, 3, 2, 1, 3));
-        List<Integer> input4 = new ArrayList<>(List.of(1, 2, 3, 1, 2, 3, 3, 3));
+        List<Integer> input = new ArrayList<>(List.of(3, 3, 2, 1, 3));
+        List<Integer> input1 = new ArrayList<>(List.of(1, 2, 3, 1, 2, 3, 3, 3));
         List<Integer> input3 = new ArrayList<>(List.of(1, 2, 3, 1, 2, 3, 3, 3, 4, 4, 4, 4, 4, 4));
-        List<Integer> input = new ArrayList<>(List.of(
+        List<Integer> input4 = new ArrayList<>(List.of(
                 10, 27, 9, 10, 100, 38, 30, 32, 45, 29, 27, 29,
                 32, 38, 32, 38, 14, 38, 29, 30, 63, 29, 63, 91, 54, 10, 63));      // Correct = 23 != My = 19
 
-        List<Integer> indexes = new ArrayList<>();               // Какво става ако два елемента са с еднакви max?
-        List<Integer> elementWithThisIndex = new ArrayList<>();
+        List<Integer> indexes = new ArrayList<>();
+        List<Integer> elementWithMaxIndex = new ArrayList<>();
         List<Integer> elements = new ArrayList<>();
-        int countOfMatches = 0, counter = 0, countMaxIndex = 0, max = Integer.MIN_VALUE;
+        int countOfMatches = 0, counter = 0, countMaxIndex = 0, result = 0, max = Integer.MIN_VALUE;
 
         for (int i = 0; i < input.size(); i++) {
             countOfMatches = 0;
@@ -47,9 +47,13 @@ public class HREqualizetheArray {
             if (indexes.get(i) < max) counter++;
             if (indexes.get(i) == max) {
                 countMaxIndex++;
-                elementWithThisIndex.add(input.get(i));
+                if (input.get(i) != input.get(i + 1)) elementWithMaxIndex.add(input.get(i));
             }
         }
+
+        if (countMaxIndex % 2 == 0 && elementWithMaxIndex.size() % 2 == 0) {
+            result = counter + elementWithMaxIndex.size() / 2;
+        } else result = counter;
 
         for (int el : input) System.out.print(el + " ");
         System.out.println();
@@ -57,9 +61,9 @@ public class HREqualizetheArray {
         System.out.println();
         for (int el : elements) System.out.print(el + " ");
         System.out.println();
-        System.out.println(counter);
+        System.out.println(result);
         System.out.println(countMaxIndex);
-        System.out.println(elementWithThisIndex);
+        System.out.println(elementWithMaxIndex);
 
     }
 }
