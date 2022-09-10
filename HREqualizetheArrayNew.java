7571432926891825135a9373b7b34cc521a2633b
12z3 <77529497+12z3@users.noreply.github.com>
2022-09-10 12:55:09 +0300

    Add files via upload

---
+++ HREqualizetheArrayNew.java
@@ -0,0 +1,56 @@
+package hackerRank;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class HREqualizetheArrayNew {
+    /**
+     * @Problem: Given an array of integers,
+     * determine the minimum number of elements to delete to leave only elements of equal value.
+     * @Web: <a href="https://www.hackerrank.com/challenges/">...</a>
+     * equality-in-a-array/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+     * @Discussions: <a href="https://www.hackerrank.com/challenges/equality-in-a-array/forum">...</a>
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> input1 = new ArrayList<>(List.of(1, 2, 3, 1, 2, 2, 3, 3, 1, 3));
+        List<Integer> input7 = new ArrayList<>(List.of(1, 5, 8, 8, 8, 1, 5));
+        List<Integer> input = new ArrayList<>(List.of(1, 5, 8, 8, 8, 1, 5, 1));
+
+        List<Integer> input2 = new ArrayList<>(List.of(1, 2, 2, 3));
+        List<Integer> input5 = new ArrayList<>(List.of(3, 3, 2, 1, 3));
+        List<Integer> input6 = new ArrayList<>(List.of(1, 2, 3, 1, 2, 3, 3, 3));
+        List<Integer> input3 = new ArrayList<>(List.of(1, 2, 3, 1, 2, 2, 3, 3, 1));
+        List<Integer> input4 = new ArrayList<>(List.of(1, 2, 3, 1, 2, 3, 3, 3, 4, 4, 4, 4, 4, 4));
+        List<Integer> input8 = new ArrayList<>(List.of(
+                10, 27, 9, 10, 100, 38, 30, 32, 45, 29, 27, 29,
+                32, 38, 32, 38, 14, 38, 29, 30, 63, 29, 63, 91, 54, 10, 63));
+
+        List<Integer> counter = new ArrayList<>();
+        int max = Integer.MIN_VALUE, countMax = 0;
+
+        for (int i = 0; i < input.size(); i++) {
+            countMax = 0;
+            for (int j = 0; j < input.size(); j++) {
+                if (Objects.equals(input.get(i), input.get(j))) countMax++;
+            }
+            counter.add(countMax);
+        }
+
+        for (int i = 0; i < counter.size(); i++) {
+            if (counter.get(i) > max) max = counter.get(i);
+        }
+
+        int finalMax1 = max;
+        counter.removeIf(el -> el == finalMax1);
+
+        System.out.println(counter);
+        System.out.println(counter.size());
+
+    }
+}
+
+
