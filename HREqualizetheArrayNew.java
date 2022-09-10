dc24407148a52aeb90e089cb35e06a1e2fc9ae82
12z3 <77529497+12z3@users.noreply.github.com>
2022-09-10 13:28:44 +0300

    Add files via upload

--- HREqualizetheArrayNew.java
+++ HREqualizetheArrayNew.java
@@ -18,7 +18,7 @@ public class HREqualizetheArrayNew {
 
         List<Integer> input1 = new ArrayList<>(List.of(1, 2, 3, 1, 2, 2, 3, 3, 1, 3));
         List<Integer> input7 = new ArrayList<>(List.of(1, 5, 8, 8, 8, 1, 5));
-        List<Integer> input = new ArrayList<>(List.of(1, 5, 8, 8, 8, 1, 5, 1));
+        List<Integer> input = new ArrayList<>(List.of(1, 5, 8, 8, 8, 1, 5, 1));         //  5 != 2 - My
 
         List<Integer> input2 = new ArrayList<>(List.of(1, 2, 2, 3));
         List<Integer> input5 = new ArrayList<>(List.of(3, 3, 2, 1, 3));
@@ -27,18 +27,20 @@ public class HREqualizetheArrayNew {
         List<Integer> input4 = new ArrayList<>(List.of(1, 2, 3, 1, 2, 3, 3, 3, 4, 4, 4, 4, 4, 4));
         List<Integer> input8 = new ArrayList<>(List.of(
                 10, 27, 9, 10, 100, 38, 30, 32, 45, 29, 27, 29,
-                32, 38, 32, 38, 14, 38, 29, 30, 63, 29, 63, 91, 54, 10, 63));
-
-        List<Integer> counter = new ArrayList<>();
-        int max = Integer.MIN_VALUE, countMax = 0;
-
-        for (int i = 0; i < input.size(); i++) {
+                32, 38, 32, 38, 14, 38, 29, 30, 63, 29, 63, 91, 54, 10, 63));           // 23 != 19 - My
+                                                                                        // (1, 5, 8, 8, 8, 1, 5, 1)
+        List<Integer> counter = new ArrayList<>();                                      // (1, 5, 8, 8, 8, 1, 5, 1)
+        int max = Integer.MIN_VALUE, countMax = 0;                              // 1 и 8 са с еднакви повторения, но
+                                                                    // от резултата трябва да се премахне само или
+        for (int i = 0; i < input.size(); i++) {                    // повторенията на 1-а или тези на 8-а.
             countMax = 0;
             for (int j = 0; j < input.size(); j++) {
                 if (Objects.equals(input.get(i), input.get(j))) countMax++;
             }
             counter.add(countMax);
         }
+
+        System.out.println(counter);
 
         for (int i = 0; i < counter.size(); i++) {
             if (counter.get(i) > max) max = counter.get(i);
