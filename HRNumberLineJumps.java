11225684784cb61986a4011f8471c54b89917846
12z3 <77529497+12z3@users.noreply.github.com>
2022-09-17 14:59:51 +0300

    Add files via upload

--- HRNumberLineJumps.java
+++ HRNumberLineJumps.java
@@ -9,7 +9,8 @@ public class HRNumberLineJumps {
     public static void main(String[] args) {
 
         //System.out.println( numberLineJumps(0,3,4,2));
-        System.out.println( numberLineJumps(0,2,5,3));
+        //System.out.println( numberLineJumps(0,2,5,3));
+        System.out.println( numberLineJumps(4523, 8092, 9419, 8076));
     }
 
     private static String numberLineJumps(int x1, int v1, int x2, int v2) {
@@ -17,7 +18,7 @@ public class HRNumberLineJumps {
         int s1 = 0, s2 = 0;
         boolean isYes = false, isNo = false;
 
-        while (x2 < 10000) {
+        while (x2 < 1000000000) {
             s1 = x1 + v1;
             x1 = s1;
             s2 = x2 + v2;
