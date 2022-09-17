df7911f4cf1de5008946f64fa1e57e4c80421617
12z3 <77529497+12z3@users.noreply.github.com>
2022-09-17 14:50:47 +0300

    Add files via upload

---
+++ HRNumberLineJumps.java
@@ -0,0 +1,31 @@
+package hackerRank;
+
+public class HRNumberLineJumps {
+    /**
+     * @Problem: <a href="https://www.hackerrank.com/challenges/kangaroo/problem">...</a>
+     * @Discussions: <a href="https://www.hackerrank.com/challenges/kangaroo/forum">...</a>
+     */
+
+    public static void main(String[] args) {
+
+        //System.out.println( numberLineJumps(0,3,4,2));
+        System.out.println( numberLineJumps(0,2,5,3));
+    }
+
+    private static String numberLineJumps(int x1, int v1, int x2, int v2) {
+        int cont1 = 0, count2 = 0;
+        int s1 = 0, s2 = 0;
+        boolean isYes = false, isNo = false;
+
+        while (x2 < 10000) {
+            s1 = x1 + v1;
+            x1 = s1;
+            s2 = x2 + v2;
+            x2 = s2;
+            if (s1 == s2) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+}
