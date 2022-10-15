aca5fa1d193eb8e1529454f258a8a5ddf657dd5b
12z3 <77529497+12z3@users.noreply.github.com>
2022-10-15 08:50:44 +0300

    Add files via upload

--- JumpingOnTheClouds.java
+++ JumpingOnTheClouds.java
@@ -4,31 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JumpingOnTheClouds {
+
+    /**
+     * @Problem: https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem?utm_campaign=challenge
+     * -recommendation&utm_medium=email&utm_source=24-hour-campaign
+     */
     public static void main(String[] args) {
-        List<Integer> input1 = new ArrayList<>(List.of(0, 1, 0, 0, 0, 1, 0));
-        List<Integer> input2 = new ArrayList<>(List.of(0, 0, 1, 0, 0, 1, 0));
-        List<Integer> input = new ArrayList<>(List.of(0, 0, 0, 1, 0, 0));                 // 0 2 4 5 6 - 4
-        List<Integer> input3 = new ArrayList<>(List.of(0, 0, 0, 0, 1, 0));
-        List<Integer> result = new ArrayList<>();
-        int jump = 0;
 
-        for (int i = 0; i < input.size();) {
-            if (i == 0 && input.get(0) == 0) result.add(0);
-            if (i + 2 < input.size() && input.get(i + 2) == 0) {
-                i += 2;
-                result.add(i);
-                jump ++;
-            } else if (i < input.size()){
-                i++;
-                result.add(i);
-                jump ++;
-            }
-        }
+        int[] c1 = {0, 0, 1, 0, 0, 1, 1, 0};
+        int[] c = {1, 1, 1, 0, 1, 1, 0, 0, 0, 0}; // 80 ?
+        int[] c3 = {0, 0, 1, 0};
 
-        for (int el : result) System.out.print(el + " ");
-        System.out.println();
-        System.out.println(jump);
+        System.out.println(extracted(c, 3));
     }
 
-
+    private static int extracted(int[] c, int k) {
+        List<Integer> lst = new ArrayList<>();
+        List<Integer> lst1 = new ArrayList<>();
+        int n = c.length;
+        int health = 100;
+        for (int i = 0; i < c.length; i += k) {
+            if (c[i] == 0) {
+                health -= 1;
+            } else health -= 3;
+            lst.add(i);
+            lst1.add(c[i]);
+        }
+        System.out.println(lst);
+        System.out.println(lst1);
+        return health;
+    }
 }
