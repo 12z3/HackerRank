1e7d38b38a50238a61d27fd84629c5c077585300
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-07 21:49:17 +0200

    Add files via upload

--- one.java
+++ one.java
@@ -10,13 +10,20 @@ public class one {
         int[] a = {1, 2, 3, 6, 7, 8};
 
         for (int i = 0; i < a.length - 1; i++) {
+            ls = new ArrayList<>();
             int el1 = a[i], el2 = a[i + 1], index = i + 1;
+            boolean add = false;
 
             while (compare(el1, el2) && index < a.length - 1){
-                ls.add(el1);
-                ls.add(el2);
+                if (add){
+                    ls.add(el2);
+                }else {
+                    ls.add(el1);
+                    ls.add(el2);
+                }
+                add = true;
                 el1 = a[index];
-                el2 = a[index] + 1;
+                el2 = a[index + 1];
                 index++;
             }
         }
