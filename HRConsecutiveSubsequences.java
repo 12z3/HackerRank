24bb938bef8734d72c2bac62b9bc33489d93e04f
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-07 20:47:21 +0200

    Add files via upload

--- HRConsecutiveSubsequences.java
+++ HRConsecutiveSubsequences.java
@@ -12,23 +12,36 @@ public class HRConsecutiveSubsequences {
      * Jigar got a sequence of n positive integers as his birthday present!
      * He likes consecutive subsequences whose sum is divisible by k.
      * He asks you to write a program to count them for him.
+     *
      * </p>
-     * 1, 2, 3, 4, 1};
+     * {1, 2, 3, 4, 1};
      * 3
      * 1 2
      * 1 2 3
      * 2 3 4
+     * <p>
+     * {1 2 1 2 1 2}
+     * 2
+     * 2
+     * 2
+     * 1 2 1
+     * 1 2 1
+     * 1 2 1 2
+     * 2 1 2 1
+     * 1 2 1 2
+     * 2 1 2 1 2
+     * </p>
      */
-                                                                             // Оправи си кода <-
+    // Оправи си кода <-
     public static void main(String[] args) {
-        int[] inp1 = {1, 2, 3, 4, 1}; // num = 3;       ... О'k
-        int[] inp = {1, 2, 1, 2, 1, 2};  // num = 2;    ... No O'k
-        int num = 2;
+        int[] inp = {1, 2, 3, 4, 1}; // num = 3;       ... О'k
+        int[] inp1 = {1, 2, 1, 2, 1, 2};  // num = 2;    ... No O'k
+        int num = 3;
 
         subsequences(inp, num);
     }
 
-    private static void subsequences(int[] inp, int k) {
+    private static void subsequences(int[] inp, int num) {
         List<Integer> result = null;
         int sum, count = 0;
 
@@ -38,23 +51,24 @@ public class HRConsecutiveSubsequences {
             int index = i + 1;
             boolean condition = true, isBreak = false, add = false;
 
-            if (inp[i] % k == 0) System.out.println(inp[i]);
+            if (inp[i] % num == 0) System.out.println(inp[i]);
 
-            if (inp[i] + 1 == inp[i + 1]) {
+            if (inp[i] + 1 == inp[i + 1]) {                                       // ? -> 1, 2
                 sum = inp[i] + inp[i + 1];
-                if (((sum % 3) == 0)) {
+                if (((sum % num) == 0)) {                                         // sum = 1 + 2
                     result.add(inp[i]);
                     result.add(inp[i + 1]);
                     add = true;
                     count++;
                     System.out.println(result);
-                }
+                } //else condition = false;
             } else continue;
 
-            while (condition && index < inp.length - 1) {
-                if (inp[index] + 1 == inp[index + 1]) {              // i = 0(1), index = 1(2), index + 1 = 2(3)
-                    sum += inp[index + 1];
-                    if ((sum % k) == 0) {
+
+            while (index < inp.length - 1) {
+                if (inp[index] + 1 == inp[index + 1]) {          //  ? -> 2,3; i = 0(1), index = 1(2), index + 1 = 2(3)
+                    sum += inp[index + 1];                                        // sum = (1 + 2) + 3
+                    if ((sum % num) == 0) {
                         if (!add) {
                             result.add(inp[i]);
                             result.add(inp[i + 1]);
@@ -65,10 +79,7 @@ public class HRConsecutiveSubsequences {
                         System.out.println(result);
                     } else break;
                     index++;
-                } else {
-                    condition = false;
-                    break;
-                }
+                } else break;
             }
         }
 
