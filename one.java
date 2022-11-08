6f941bd86f5ada99b0932740215031d6d5588031
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-08 17:13:32 +0200

    Add files via upload

--- one.java
+++ one.java
@@ -5,22 +5,32 @@ import java.util.List;
 
 public class one {
     public static void main(String[] args) {
-        List<Integer> ls ;
+        List<Integer> ls;
 
-        int[] a = {1, 2, 3, 6, 7, 8};
+        int[] a1 = {1, 2, 3, 6, 7, 8};
+        int[] a = {1, 2, 3, 4, 1};
+        int count = 0;
+
 
         for (int i = 0; i < a.length - 1; i++) {
             ls = new ArrayList<>();
             int el1 = a[i], el2 = a[i + 1], index = i + 1;
             boolean add = false;
 
-            while (compare(el1, el2) && index < a.length - 1){
-                if (add){
+            while (compare(el1, el2) && index < a.length - 1) {
+                if (add) {
                     ls.add(el2);
-                }else {
+                    count++;
+                } else {
                     ls.add(el1);
                     ls.add(el2);
+                    count++;
                 }
+
+                if ( (index == a.length - 2) && (a[a.length - 1] == a[a.length - 2] + 1)) {
+                    ls.add(a[a.length - 1]);
+                }
+
                 add = true;
                 el1 = a[index];
                 el2 = a[index + 1];
@@ -29,6 +39,7 @@ public class one {
             }
         }
 
+        System.out.println(count);
 //        System.out.println(ls);                          // [1, 2, 2, 3, 3, 4, 6, 7, 2, 3, 3, 4, 6, 7, 6, 7]
     }
 
