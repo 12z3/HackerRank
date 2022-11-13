01201765e4d65d3f89ce85a705f125dbdd4ba1c3
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-13 13:37:26 +0200

    Add files via upload

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -8,33 +8,44 @@ public class MaximumSubarray {
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>(List.of(-1, 2, 3, -4, 5, 10));
 
-       // Collections.sort(arr);
-        System.out.println(arr);
+        System.out.println(maximumSubarray(arr));
+    }
 
+    private static List<Integer> maximumSubarray(List<Integer> arr){
         List<Integer> maxLs = new ArrayList<>();
+        List<Integer> idx = null;
         List<Integer> ls;
-        int sum = 0, max = Integer.MIN_VALUE, index;
+        int sum = 0, max = Integer.MIN_VALUE, index, sumIdx = 0;
+
 
         for (int i = 0; i < arr.size(); i++) {
             ls = new ArrayList<>();
+            idx = new ArrayList<>();
             sum = 0;
             index = i + 1;
             sum = arr.get(i);
+
+            idx.add(i);
             ls.add(arr.get(i));
             while (index < arr.size()){
                 ls.add(arr.get(index));
                 sum  += arr.get(index);
-                System.out.println(sum);
+
+                
+                idx.add(index);
                 index++;
             }
             if (sum > max) {
                 max = sum;
                 maxLs = ls;
             }
-            System.out.println("--------------------");
         }
 
-
-        System.out.println(maxLs + " = " + max);
+        for (int i = 0; i < maxLs.size(); i++) {
+            sumIdx += i;
+        }
+        System.out.println(maxLs + " = " + max + " / " + sumIdx);
+        System.out.println(idx);
+        return maxLs;
     }
 }
